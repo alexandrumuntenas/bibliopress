61d5a3269233d9570118ad212e3cae72e36a891f
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-09-26 18:43:03 +0200

    Función delete.php creada

--- bp-admin/functions/delete.php
+++ bp-admin/functions/delete.php
@@ -1,1 +1,7 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT']. '/bp-config.php';
+$loggedin = $_COOKIE["loggedin"];
+$id=$_REQUEST['id'];
+$query = "DELETE FROM $tableMySQL WHERE id=$id"; 
+$result = mysqli_query($databaseconnection,$query);
+?>
