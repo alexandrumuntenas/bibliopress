78892c7514784c6db2e3e82fd28ab82894036b8d
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-09-26 13:18:05 +0200

    Añadir formulario para añadir registros a la base de datos de Bookpress

--- bp-admin/panel.php
+++ bp-admin/panel.php
@@ -63,24 +63,74 @@ $loggedin = $_COOKIE["loggedin"];
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
             Añadir nuevo registro
             </button>
-
+            <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header ">
                     <h5 class="modal-title" id="staticBackdropLabel">Añadir nuevo registro</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
-                </div>
+                <form id="form_1388" class="appnitro"  method="post" action="">				
+                <ul>
+                    
+                <li id="li_1">
+                <label class="description" for="element_1">Título del libro </label>
+                <div>
+                    <input id="element_1" name="element_1" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_2">
+                <label class="description" for="element_2">Autor </label>
+                <div>
+                    <input id="element_2" name="element_2" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_3">
+                <label class="description" for="element_3">ISBN </label>
+                <div>
+                    <input id="element_3" name="element_3" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_4">
+                <label class="description" for="element_4">Editorial </label>
+                <div>
+                    <input id="element_4" name="element_4" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_5">
+                <label class="description" for="element_5">Año de Publicación </label>
+                <div>
+                    <input id="element_5" name="element_5" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_6">
+                <label class="description" for="element_6">Ejemplar </label>
+                <div>
+                    <input id="element_6" name="element_6" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_7">
+                <label class="description" for="element_7">Ubicación </label>
+                <div>
+                    <input id="element_7" name="element_7" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_8" >
+                <label class="description" for="element_8">Disponibilidad </label>
+                <span>
+                    <input id="element_8_1" name="element_8_1" class="element checkbox" type="checkbox" value="1" />
+        <label class="choice" for="element_8_1">Sí</label>
+        <input id="element_8_2" name="element_8_2" class="element checkbox" type="checkbox" value="1" />
+        <label class="choice" for="element_8_2">No</label>
+
+                </span> 
+                </li>
+                </ul>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Guardar</button>
+                    <input id="saveForm" class="btn btn-primary" type="submit" name="submit" value="Publicar" />
+                </div>
+                </form>	
                 </div>
                 </div>
+            </div>
             </div>
             </div>
             <table>
