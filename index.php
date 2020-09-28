fa820faf8342e435080ad2520be54dea28225863
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-09-28 18:30:40 +0200

    Trabajo en la nueva interfaz del inicio, basado en epublibre.org

--- index.php
+++ index.php
@@ -23,15 +23,7 @@ require 'bp-config.php';
                 if ($resultado->num_rows > 0) {
                     //datos de cada columna
                     while($row = $resultado->fetch_assoc()) {
-                        echo '<div class="cardse card-body">
-                        <h5><strong>' . $row["TITULO"] . '</strong></h5>
-                        <p><em>' . $row["AUTOR"] . '</em></p>
-                        <p><strong>ISBN</strong> <em>' . $row["ISBN"] . '</em></p>
-                        <p><strong>Ubicación</strong> <em>' . $row["UBICACION"] . '</p></em>
-                        <p><strong>Ejemplar</strong> <em>' . $row["EJEMPLAR"] . ' </em></p>
-                        <p><strong>Año de Publicación</strong> <em>' . $row["ANOPUB"] . '</em></p>
-                        <p><strong>Editorial</strong> <em>' . $row["EDITORIAL"] . '</em></p>
-                        </div>';
+                        echo '';
                     }
                 } else {
                     echo "No existe ningún registro";
