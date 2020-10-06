e6fada3848025e7b2ba1d2662b4cf12cbc0b9793
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-06 18:26:27 +0200

    Preparación del ajax.php para poner un link directo a la hora de hacer click sobre la búsqueda

--- bp-include/ajax.php
+++ bp-include/ajax.php
@@ -2,7 +2,7 @@
 require '../bp-config.php';
 if (isset($_POST['search'])) {
    $busqueda = $_POST['search'];
-   $Queray = "SELECT TITULO FROM bp_catalogo WHERE TITULO LIKE '%$busqueda%' LIMIT 5";
+   $Queray = "SELECT TITULO, ID FROM bp_catalogo WHERE TITULO LIKE '%$busqueda%' LIMIT 5";
    $ExecQuery = MySQLi_query($databaseconnection, $Queray);
    echo '
 <ul class="list-group">
