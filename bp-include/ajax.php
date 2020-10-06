829b6b1a3c3775a75bd4dcaaa2e849aa50de08e7
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-06 18:15:37 +0200

    Paso (2/5) de la búsqueda con Ajax en en catálogo!

---
+++ bp-include/ajax.php
@@ -0,0 +1,19 @@
+<?php
+require '../bp-config.php';
+if (isset($_POST['search'])) {
+   $busqueda = $_POST['search'];
+   $Queray = "SELECT TITULO FROM bp_catalogo WHERE TITULO LIKE '%$busqueda%' LIMIT 5";
+   $ExecQuery = MySQLi_query($databaseconnection, $Queray);
+   echo '
+<ul class="list-group">
+   ';
+   while ($Result = MySQLi_fetch_array($ExecQuery)) {
+?>
+   <li class="list-group-item lives" onclick='fill("<?php echo $Result['TITULO']; ?>")'>
+   <a>
+       <?php echo $Result['TITULO']; ?>
+   </li></a>
+   <?php
+}}
+?>
+</ul>
