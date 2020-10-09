ff857fd53758571febc3819f37ed014d2394f47a
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-09 22:02:59 +0200

    BBDD de usuarios dinámico

--- bp-config.php
+++ bp-config.php
@@ -10,13 +10,11 @@ $formatofecha = "Y";
 $sql = "SELECT TITULO, AUTOR, ISBN, EDITORIAL, UBICACION, ANOPUB, EJEMPLAR, ID, DESCRIPCION FROM $tableMySQL";
 $resultado = $databaseconnection->query($sql);
 
-$lectorsql = "SELECT * FROM `bp_estudiantes`";
+$bbddusuarios = "bp_usuarios";
+$lectorsql = "SELECT * FROM `$bbddusuarios`";
 $lectorresultado = $databaseconnection->query($lectorsql);
 
 //Cookies para futuro sistema de login
-$cnombre = "loggedin";
-$cvalor = "0";
-setcookie($cnombre, $cvalor, time() + (3600000), "/"); // 86400 = 1 dia
 
 $dformat = date($formatofecha);
 
