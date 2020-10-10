ff420c53f2548d848d5d105ede26693a3c2cae41
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-10 23:20:12 +0200

    Añadir título dinámico y Añadir nuevos enlaces para el sistema de préstamos

--- bp-include/menu.php
+++ bp-include/menu.php
@@ -1,18 +1,27 @@
 <?php $sessionlog = $_COOKIE['loggedin'];
 $sessionus = $_COOKIE['usuario']; 
 echo '<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <a class="navbar-brand" href="/">Biblioteca</a>
+    <a class="navbar-brand" href="/">Biblioteca del ' . $sname. '</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">';
         if ($sessionlog == 1) {
-            echo '<li class="nav-item">
-            <a class="nav-link" href="/"><i class="fas fa-star"></i> Catálogo</span></a>
+            if ($sessionclass == 1){
+                echo '<li class="nav-item">
+                <a class="nav-link" href="/"><i class="fas fa-star"></i> Catálogo</span></a>
+            </li><li class="nav-item">
+            <a class="nav-link" href="/bp-admin/lectores.php"><i class="fas fa-book-reader"></i> Lectores</span></a>
         </li><li class="nav-item">
-        <a class="nav-link" href="/bp-admin/lectores.php"><i class="fas fa-book-reader"></i> Lectores</span></a>
+        <a class="nav-link" href="/bp-admin/prestamos.php"><i class="fas fa-people-carry"></i> Préstamos</span></a>
     </li>';
+            } else {
+                echo '<li class="nav-item">
+            <a class="nav-link" href="/"><i class="fas fa-star"></i> Inicio</span></a>
+        </li>';
+            }
+            
         } else {
             echo '        <li class="nav-item">
             <a class="nav-link" href="/"><i class="fas fa-star"></i> Inicio</span></a>
