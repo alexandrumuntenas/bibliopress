ff80bbb5039c4f58bf6dbb202c676b3aff39182d
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-10 16:30:13 +0200

    Creación de sección dinámica. Acceso a este dependiendo de si estás logueado y de tu permiso. (Tipo de perfil). Algunos cambios de título.

--- bp-admin/index.php
+++ bp-admin/index.php
@@ -1,30 +1,32 @@
 <?php
 //Importación de datos
 require '../bp-config.php';
-
-$sessionlogged = $_COOKIE['loggedin'];
-$sessionclass = $_COOKIE['perm'];
 ?>
 <html>
-    <?php require '../bp-include/head.html';?>
-    <body class="headerlogin">
-        <header>
+<?php require '../bp-include/head.html'; ?>
+
+<body class="headerlogin">
+    <header>
         <title>
-        <?php echo 'Biblioteca del ' . $sname;?>
+            <?php echo 'Biblioteca del ' . $sname; ?>
         </title>
-            <div class="wrapper">
-            <?php require '../bp-include/menu.php';?>
-            <?php if($sessionlogged == 1){ echo '
+        <div class="wrapper">
+            <?php require '../bp-include/menu.php'; ?>
+            <?php if ($sessionlogged == 1) {
+                echo '
             <div class="header">
-                <h2 class="centered">Dashboard</h2>
-            </div>';} else{ echo '
+                <h2 class="centered">Área Personal</h2>
+            </div>';
+            } else {
+                echo '
                 <div class="header">
                     <h2 class="centered">Iniciar Sesión</h2>
-                </div>';};?>
-        </header>
-            <?php if($sessionlogged == 1){
-                if($sessionclass == 1){
-                    echo '<section class="section"><center>
+                </div>';
+            }; ?>
+    </header>
+    <?php if ($sessionlogged == 1) {
+        if ($sessionclass == 1) {
+            echo '<section class="section flex-column"><center>
                     <div class="btn-group" role="group">
                     <a href="index.php" type="button" class="btn btn-primary">Inicio</a>
                     <div class="btn-group" role="group">
@@ -39,27 +41,26 @@ $sessionclass = $_COOKIE['perm'];
                     <a href="lectores.php" type="button" class="btn btn-secondary">Lectores</a>
                     </div>
                     </center></section>';
-                }
-                else{
-                    echo '<section class="section"><center>
+        } else {
+            echo '<section class="section"><center>
                     <div class="btn-group" role="group">
                     <a href="index.php" type="button" class="btn btn-primary">Inicio</a>
                     <a href="listadedeseos.php" class="btn btn-secondary">Lista de deseos</a>
-                    <a href="lectores.php" type="button" class="btn btn-secondary">Lectores</a>
+                    <a href="unlogger.php" type="button" class="btn btn-danger">Cerrar Sesión</a>
                     </div>
                     </center></section>';
-                }
-                }
-                else{
-                    echo "<section class='section'><div class='viewer'><form name='loginform' id='loginform' method='post' action='logger.php'>
+        }
+    } else {
+        echo "<section class='section'><div class='viewer'><form name='loginform' id='loginform' method='post' action='logger.php'>
                     <p><strong><label>Usuario</strong><br /><input type='text' name='usuario' id='user_login' class='input' value='' /></label></p>
                     <p><strong><label>PIN</strong><br /><input type='password' name='contrasena' id='user_pass' class='input' value=''  /></label></p>
                     <p class='submit'><input class='btn btn-primary' type='submit' name='pwd_submit' id='pwd_submit' value='Iniciar sesi&oacute;n' /></p></form></div></section>";
-                }
-            ?>
-        <footer class="page-footer bg-primary">
+    }
+    ?>
+    <footer class="page-footer bg-primary">
         <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
         </div>
-        </footer>
-    </body>
+    </footer>
+</body>
+
 </html>
