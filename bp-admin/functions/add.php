3546729236c7d8f7e889c6eb38dc9e719393ecca
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-10 16:30:57 +0200

    Creación de sección dinámica. Acceso a este dependiendo de si estás logueado y de tu permiso. (Tipo de perfil)

--- bp-admin/functions/add.php
+++ bp-admin/functions/add.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
+<html>
 <?php
 //Importación de datos
-require $_SERVER['DOCUMENT_ROOT']. '/bp-config.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/bp-config.php';
 $loggedin = $_COOKIE["loggedin"];
 
 //Tomar datos de formulario añadir registro desde catalogo.php
@@ -24,16 +26,24 @@ $ubicacion = "13";*/
 
 $insert = "INSERT INTO $tableMySQL(ANOPUB, AUTOR, EJEMPLAR, EDITORIAL,TITULO, UBICACION, ISBN, DESCRIPCION) VALUES ('$anopub','$autor','$ejemplar','$editorial','$titulo','$ubicacion','$ISBN','$descripcion')";
 $databaseconnection->query($insert);
-
-//Futura interfaz de información
+require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/head.html'; ?>
 ?>
-<!DOCTYPE html>
-<html>
-    <?php require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/head.html';?>
-    <body>
-        <header>
-            <div class="wrapper">
-            <?php require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';?>
+<?php
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+        echo '<body>';
+    } else {
+        echo '<body class="err404">';
+    }
+} else {
+    echo '<body class="err404">';
+} ?>
+<?php
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+        echo '<header><div class="wrapper">';
+        require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';
+        echo '
         </header>
             <div class="header">
                 <h2 class="centered">Dashboard</h2>
@@ -48,40 +58,63 @@ $databaseconnection->query($insert);
                        echo "<table>
                             <thead>
                                 <tr>
-                                    <th><h5><strong>" . $titulo . "</strong></h5></th>
+                                    <th><h5><strong>' . $titulo . '</strong></h5></th>
                                     <th></th>
                                     <th></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td><p><em>" . $autor . "</em></p></td>
+                                    <td><p><em>' . $autor . '</em></p></td>
                                     <td></td>
                                     <td></td>
                                 </tr>
                                 <tr>
-                                    <td><p><strong>Sipnósis <br></strong> <em>" . $descripcion . "</em></p></td>
+                                    <td><p><strong>Sipnósis <br></strong> <em>' . $descripcion . '</em></p></td>
                                 </tr>
                                 <tr>
-                                    <td><p><strong>ISBN</strong> <em>" . $ISBN . "</em></p></td>
-                                    <td><p><strong>Ubicación</strong> <em>" . $ubicacion . "</td>
+                                    <td><p><strong>ISBN</strong> <em>' . $ISBN . '</em></p></td>
+                                    <td><p><strong>Ubicación</strong> <em>' . $ubicacion . '</td>
                                 </tr>
                                 <tr>
-                                    <td><p><strong>Editorial</strong> <em>" . $editorial . "</em></p></td>
-                                    <td><p><strong>Año de Publicación</strong> <em>" . $anopub . "</td>
-                                    <td><p><strong>Ejemplar</strong> <em>" . $ejemplar . "</td>
+                                    <td><p><strong>Editorial</strong> <em>' . $editorial . '</em></p></td>
+                                    <td><p><strong>Año de Publicación</strong> <em>' . $anopub . '</td>
+                                    <td><p><strong>Ejemplar</strong> <em>' . $ejemplar . '</td>
                                 </tr>
                             </tbody>
-                        </table> </div>"; ?>
+                        </table> </div>
                         <div class="btn-group" role="group">
                             <a class="btn btn-primary" href="/bp-admin/catalogo.php">Volver</a>
                             <input type = "button" class="btn btn-success" value = "Imprimir página" onclick = "window.print()" />
                         </div>
             
-        </section>
-        <footer class="page-footer bg-primary">
-        <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
-        </div>
-        </footer>
-    </body>
+        </section>';
+    } else {
+
+        echo '<section class="error-container">
+                        <span><span>4</span></span>
+                        <span>0</span>
+                        <span><span>4</span></span>
+                      </section>
+                      <center>
+                        <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                        <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+    }
+} else {
+    echo '<section class="error-container">
+                    <span><span>4</span></span>
+                    <span>0</span>
+                    <span><span>4</span></span>
+                  </section>
+                  <center>
+                  <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                  <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+};
+?>
+<footer class="page-footer bg-primary">
+    <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
+    </div>
+</footer>
+</body>
+
 </html>
