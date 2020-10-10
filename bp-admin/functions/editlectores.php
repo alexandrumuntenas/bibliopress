8b71a5b32d375cf99d4eaf90c45d0b103d0b2989
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-10 17:10:05 +0200

    Creación de sección dinámica. Acceso a este dependiendo de si estás logueado y de tu permiso. (Tipo de perfil)

--- bp-admin/functions/editlectores.php
+++ bp-admin/functions/editlectores.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/bp-config.php';
 $USUARIO = $_REQUEST['USUARIO'];
@@ -5,18 +7,21 @@ $query = "SELECT * FROM `$bbddusuarios` WHERE `USUARIO` = '" . $USUARIO . "'";
 $result = mysqli_query($databaseconnection, $query);
 $row = mysqli_fetch_assoc($result);
 $loggedin = $_COOKIE["loggedin"];
-?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <?php require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/head.php';?>
-
-<body class="headerlogin">
-    <div class="form">
-    <?php require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';?>
-        <?php
+require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/head.php';
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+        echo '<body>';
+    } else {
+        echo '<body class="err404">';
+    }
+} else {
+    echo '<body class="err404">';
+}
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+        echo '
+    <div class="form">';
+        require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';
         $status = "";
         if (isset($_POST['new']) && $_POST['new'] == 1) {
             $USUARIO = $_REQUEST['USUARIO'];
@@ -39,7 +44,7 @@ $loggedin = $_COOKIE["loggedin"];
                         <table style="overflow-x: scroll;">
                             <thead>
                                 <tr>
-                                    <th><input type="hidden" name="new" value="1" /><input name="USUARIO" type="hidden" value="' . $row['USUARIO'] . '" /><h5>Nombre <input type="text" name="nombre" placeholder="Escribe el nombre del lector" required value="'. $row['NOMBRE'] . '" /></h5><h5>Apellidos <input type="text" name="APELLIDOS" placeholder="Escribe los apellidos del lector" required value="'. $row['APELLIDOS'] . '" /></h5></th>
+                                    <th><input type="hidden" name="new" value="1" /><input name="USUARIO" type="hidden" value="' . $row['USUARIO'] . '" /><h5>Nombre <input type="text" name="nombre" placeholder="Escribe el nombre del lector" required value="' . $row['NOMBRE'] . '" /></h5><h5>Apellidos <input type="text" name="APELLIDOS" placeholder="Escribe los apellidos del lector" required value="' . $row['APELLIDOS'] . '" /></h5></th>
                                     <th></th>
                                     <th></th>
                                 </tr>
@@ -54,15 +59,37 @@ $loggedin = $_COOKIE["loggedin"];
                         </table>
                     </form>
                 </div>';
-        }; ?>
+        };
+    } else {
 
-        <div>
-        </div>
+        echo '<section class="error-container">
+                                    <span><span>4</span></span>
+                                    <span>0</span>
+                                    <span><span>4</span></span>
+                                  </section>
+                                  <center>
+                                    <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                                    <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+    }
+} else {
+    echo '<section class="error-container">
+                                <span><span>4</span></span>
+                                <span>0</span>
+                                <span><span>4</span></span>
+                              </section>
+                              <center>
+                              <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                              <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+};
+?>?>
+
+<div>
+</div>
+</div>
+<footer class="page-footer bg-primary">
+    <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
     </div>
-    <footer class="page-footer bg-primary">
-        <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
-        </div>
-        </footer>
+</footer>
 </body>
 
 </html>
