24b28f4d87c7bbd4faec575ddc021265e47907d8
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-10 19:14:06 +0200

    Volver a añadir la función de Añadir Registros

--- index.php
+++ index.php
@@ -36,11 +36,77 @@ if($logger == 0){
                 echo '<br><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
                 Añadir nuevo registro
                 </button>
-                <a href="functions/abies.php" class="btn btn-primary">Subir desde Abies</a>';
+                <a href="bp-admin/functions/abies.php" class="btn btn-primary">Subir desde Abies</a>';
             }
 
 
-            echo '<div id="display"></div>';
+            echo '<div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header ">
+                    <h5 class="modal-title" id="staticBackdropLabel">Añadir nuevo registro</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form id="form_1388" class="appnitro"  method="post" action="functions/add.php">				
+                <ul>
+                    
+                <li id="li_1">
+                <label class="description" for="element_1">Título del libro </label>
+                <div>
+                    <input id="element_1" name="element_1" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_2">
+                <label class="description" for="element_2">Autor </label>
+                <div>
+                    <input id="element_2" name="element_2" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_3">
+                <label class="description" for="element_3">ISBN </label>
+                <div>
+                    <input id="element_3" name="element_3" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_4">
+                <label class="description" for="element_4">Editorial </label>
+                <div>
+                    <input id="element_4" name="element_4" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_5">
+                <label class="description" for="element_5">Año de Publicación </label>
+                <div>
+                    <input id="element_5" name="element_5" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_6">
+                <label class="description" for="element_6">Ejemplar </label>
+                <div>
+                    <input id="element_6" name="element_6" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>		<li id="li_7">
+                <label class="description" for="element_7">Ubicación </label>
+                <div>
+                    <input id="element_7" name="element_7" class="element text large" type="text" maxlength="255" value=""/> 
+                </div> 
+                </li>
+                <li id="li_7">
+                <label class="description" for="element_8">Descripción </label>
+                <div>
+                    <input type="text" id="element_8" name="element_8" class="element text large" maxlength="512" value=""/>
+                </div> 
+                </li>	
+                </ul>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                    <input id="saveForm" class="btn btn-primary" type="submit" name="submit" value="Publicar" />
+                </div>
+                </form>	
+                </div>
+                </div>
+            </div>
+            </div>
+            </div><div id="display"></div>';
             ?>
             <div class="row">
             <?php
