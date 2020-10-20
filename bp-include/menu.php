3f805dd1b85f1d32d064d79c60562d4d442d152b
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-20 16:33:18 +0200

    Eliminar Nombre Dinámico

--- bp-include/menu.php
+++ bp-include/menu.php
@@ -1,7 +1,7 @@
 <?php $sessionlog = $_COOKIE['loggedin'];
 $sessionus = $_COOKIE['usuario']; 
 echo '<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <a class="navbar-brand" href="/">Biblioteca del ' . $sname. '</a>
+    <a class="navbar-brand" href="/">Biblioteca</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
