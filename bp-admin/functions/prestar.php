1bd69387ed14690d6bf406125f016e5ddec21866
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-20 16:40:36 +0200

    Crear comprobador de prestamos

--- bp-admin/functions/prestar.php
+++ bp-admin/functions/prestar.php
@@ -24,18 +24,40 @@ if ($sessionlogged == 1) {
         $fnamechecksql = "SELECT * FROM `$bbddusuarios` WHERE `FULLNAME` = '" .$FNAME. "'";
         $fnamedata = mysqli_query($databaseconnection, $fnamechecksql);
         $fnamecheck = mysqli_fetch_assoc($fnamedata);
+        $fnamecheck['USUARIO'] = $usuariocompleto;
         $query = "SELECT * FROM `$tableMySQL` WHERE `ID` = '" . $id . "'";
         $result = mysqli_query($databaseconnection, $query);
         $row = mysqli_fetch_assoc($result);
-        if($fnamecheck['FULLNAME'] == $FNAME){
+        $comprobadorsql = "SELECT * FROM `$tableMySQL` WHERE `PRESTADOA` = '".$usuariocompleto."'";
+        $comprobadordata = mysqli_query($databaseconnection, $comprobadorsql);
+        $cantidadprestada = mysqli_num_rows($comprobadordata);
+
+        if ($cantidadprestada >= 6){
+            echo '<section class="section">
+            <div>
+                <h2 class="stitle"></h2>
+            </div>
+            <div class="cardse card-body">
+                       <table>
+                            <thead>
+                                <tr>
+                                    <th><h5>Parece que ' . $apellido . ', ' . $nombre . ' tiene ya 5 préstamos activos</em><strong></strong></h5></th>
+                                </tr>
+                            </thead>
+                        </table> </div>
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-primary" href="/">Volver</a>
+                        </div>
+            
+        </section>';
+        } else {
+            if($fnamecheck['FULLNAME'] == $FNAME){
             echo 'To correcto';
             print_r($fnamecheck);
             $sql = "UPDATE `bp_catalogo` SET `DISPONIBILIDAD` = '0', `PRESTADOA` = '" .$fnamecheck['USUARIO']. "', `FECHADEV` = '" .$timestamp. "' WHERE `bp_catalogo`.`ID` = ".$id;
             $databaseconnection->query($sql);
         }
-        else {
-            echo 'Ha fallado papu';
-        }
+
         echo '
         <header>
             <div class="wrapper">';
@@ -64,7 +86,8 @@ if ($sessionlogged == 1) {
                         </div>
             
         </section>';
-    } else {
+    }}    
+    else {
 
         echo '<section class="error-container">
                                 <span><span>4</span></span>
@@ -74,8 +97,8 @@ if ($sessionlogged == 1) {
                               <center>
                                 <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
                                 <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
-    }
-} else {
+    }}
+ else {
     echo '<section class="error-container">
                             <span><span>4</span></span>
                             <span>0</span>
@@ -84,7 +107,8 @@ if ($sessionlogged == 1) {
                           <center>
                           <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
                           <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
-};
+}
+
 ?>
 <footer class="page-footer bg-primary">
     <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
