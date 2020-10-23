2191272813abe25626e5015b38dabfd377334129
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-23 23:36:43 +0200

    Creación de la función de prórroga de préstamo

---
+++ bp-admin/functions/prorroga.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<?php
+//Importación de datos
+require $_SERVER['DOCUMENT_ROOT'] . '/bp-config.php';
+$loggedin = $_COOKIE["loggedin"];
+$id = $_REQUEST['id'];
+//Tomar datos de formulario añadir registro desde lectores.php
+require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/head.php';
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+        echo '<body>';
+    } else {
+        echo '<body class="err404">';
+    }
+} else {
+    echo '<body class="err404">';
+}
+if ($sessionlogged == 1) {
+    if ($sessionclass == 1) {
+
+        $query = "SELECT * FROM `$tableMySQL` WHERE `ID` = '" . $id . "'";
+        $result = mysqli_query($databaseconnection, $query);
+        $row = mysqli_fetch_assoc($result);
+        $sql = "UPDATE `bp_catalogo` SET `FECHADEV` = '".$prorrogafecha."' WHERE `bp_catalogo`.`ID` = ".$id;
+        $databaseconnection->query($sql);
+
+        echo '
+        <header>
+            <div class="wrapper">';
+        require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';
+        echo '
+        </header>
+            <div class="bp-header">
+                <h2 class="bp-page-title">Dashboard</h2>
+            </div>
+        </header>
+        <section class="bp-section">
+            <div>
+                <h2 class="stitle"></h2>
+            </div>
+            <div class="bp-card card-body"><h5>Se ha atrasado la devolución del libro <em>' .$row['TITULO']. '</em></h5></div>
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-primary" href="/bp-admin/prestamos.php">Volver</a>
+                            <input type = "button" class="btn btn-success" value = "Imprimir página" onclick = "window.print()" />
+                        </div>
+            
+        </section>';
+    }  
+    else {
+
+        echo '<section class="error-container">
+                                <span><span>4</span></span>
+                                <span>0</span>
+                                <span><span>4</span></span>
+                              </section>
+                              <center>
+                                <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                                <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+    }}
+ else {
+    echo '<section class="error-container">
+                            <span><span>4</span></span>
+                            <span>0</span>
+                            <span><span>4</span></span>
+                          </section>
+                          <center>
+                          <h2 style="color:#FFF; margin-bottom:15px;">Parece que te has perdido</h2>
+                          <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
+}
+
+?>
+<footer class="page-footer bg-primary">
+    <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
+    </div>
+</footer>
+</body>
+
+</html>
--- bp-config.php
+++ bp-config.php
@@ -1,12 +1,13 @@
 <?php
-require('bp-settings.php'); $formatofecha = "Y"; $sql = "SELECT TITULO, AUTOR, ISBN, EDITORIAL, UBICACION, ANOPUB, EJEMPLAR, ID, DESCRIPCION, DISPONIBILIDAD, PRESTADOA FROM $tableMySQL"; $resultado = $databaseconnection->query($sql); $bbddusuarios = "bp_usuarios"; $lectorsql = "SELECT * FROM `$bbddusuarios`"; $lectorresultado = $databaseconnection->query($lectorsql); $dformat = date($formatofecha); $numerolibros = mysqli_num_rows($resultado); 
+require('bp-settings.php'); $formatofecha = "Y"; $sql = "SELECT TITULO, AUTOR, ISBN, EDITORIAL, UBICACION, ANOPUB, EJEMPLAR, ID, DESCRIPCION, DISPONIBILIDAD, PRESTADOA, FECHADEV FROM $tableMySQL"; $resultado = $databaseconnection->query($sql); $bbddusuarios = "bp_usuarios"; $lectorsql = "SELECT * FROM `$bbddusuarios`"; $lectorresultado = $databaseconnection->query($lectorsql); $dformat = date($formatofecha); $numerolibros = mysqli_num_rows($resultado); 
 $qtyprestadosql = "SELECT *  FROM ".$tableMySQL." WHERE `DISPONIBILIDAD` = 0"; $qtyprestadoquery = $databaseconnection->query($qtyprestadosql);
-$qtyprestados = mysqli_num_rows($qtyprestadoquery);
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+$qtyprestados = mysqli_num_rows($qtyprestadoquery); $aq112 = mysqli_fetch_assoc($resultado);
+//ini_set('display_errors', 0);
+//ini_set('display_startup_errors', 0);
 $sessionlogged = $_COOKIE['loggedin'];
 $sessionclass = $_COOKIE['perm'];
 $sessionus = $_COOKIE['usuario'];
 date_default_timezone_set('Europe/Berlin');
 $fecha_actual = date('m/d/Y');
 $timestamp = date("Y-m-d",strtotime($fecha_actual."+ 15 days"));
+$prorrogafecha = date("Y-m-d",strtotime($aq112['FECHADEV']."+ 15 days"));
