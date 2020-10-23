ce49e7f36c5ec79e73e557c7fd69765bac01dee5
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-23 19:57:53 +0200

    Cambiar aspecto tarjetas de búsqueda

--- bp-include/ajax.php
+++ bp-include/ajax.php
@@ -4,16 +4,12 @@ if (isset($_POST['search'])) {
    $busqueda = $_POST['search'];
    $Queray = "SELECT TITULO, ID, DESCRIPCION, AUTOR, DISPONIBILIDAD FROM bp_catalogo WHERE TITULO LIKE '%$busqueda%'LIMIT 5";
    $ExecQuery = MySQLi_query($databaseconnection, $Queray);
-   echo '<ul class="list-group">
-   ';
+   echo '<div class="row">';
    while ($Result = MySQLi_fetch_array($ExecQuery)) {
       $long = 250;
       $desc = substr($Result['DESCRIPCION'], 0, $long);
 ?>
-      <div class="row">
-         <a class="livesa" href="view.php?id=<?php echo $Result['ID']; ?>">
-            <li class="lives tarjetasbusqueda" onclick='fill("<?php echo $Result['TITULO']; ?>")'>
-
+            <div class="tarjetasbusqueda card-body">
                <h6><?php echo $Result['TITULO']; ?> por <em><?php echo $Result['AUTOR']; ?></em></h6>
                <h7></h7>
                <p><em><?php echo $desc; ?></em></p>
@@ -23,11 +19,9 @@ if (isset($_POST['search'])) {
                } else {
                   echo '<p class="badge badge-danger">Disponibilidad ✗</p><br>';
                } ?>
-            </li>
-         </a>
+               <a class="btn btn-light" href="view.php?id=<?php echo $Result['ID']; ?>">Ver más</a>
       </div>
 <?php
    }
 }
 ?>
-</ul>
