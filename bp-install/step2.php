2e903bde89053bc924078cd17df77022c828344b
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-25 15:15:26 +0100

    Añadir protector anti instalación cuando bibliopress ya está instalado

--- bp-install/step2.php
+++ bp-install/step2.php
@@ -1,3 +1,33 @@
+<?php if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/bp-settings.php")) {   
+  ?>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Nueva instalación de Bibliopress 1.0</title>
+<link rel="icon" type="image/png" href="bibliopress.png">
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+
+</head>
+<body id="main_body" >
+	
+	<img id="top" src="top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a><strong>Nueva instalación de Bibliopress 1.0</strong></a></h1>
+		<form id="form_4248" class="appnitro"  method="post" action="step2.php">
+					<div class="form_description">
+			<h2><strong>Parece que Bibliopress ya está instalado...</h2>
+			<p>Parece que ya has instalado Bibliopress. Para volver a instalarlo, por favor, primero vacía las tablas de tu base de datos antigua y elimina el archivo bp-settings.php de tu servidor</p>
+		<div id="footer">
+		</div>
+	</div>
+	</body>
+</html>
+<?php                    
+}
+else {?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -137,3 +167,4 @@
 	<img id="bottom" src="bottom.png" alt="">
 	</body>
 </html>
+    <?php } ?>
