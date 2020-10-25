4778f5db989a12f9087d0fa91cad3405915b577a
Alexmuntenas <alexandrutheodor@duoestudios.es>
2020-10-25 14:34:21 +0100

    Add Bibliopress Logo FAVICON

--- bp-include/head.php
+++ bp-include/head.php
@@ -3,6 +3,7 @@ echo "<head>
     <title>Biblioteca del " . $sname. "</title>
     <meta charset='utf-8'/>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link rel='icon' type='image/png' href='bibliopress.png'>
     <link rel='stylesheet' href='/bp-include/style.min.css'>
     <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' integrity='sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z' crossorigin='anonymous'>
     <script src='https://use.fontawesome.com/releases/v5.14.0/js/all.js' data-auto-replace-svg='nest'></script>
