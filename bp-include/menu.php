a01f095af30254f7300b6dc42b5bb9ce13041b66
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-11 22:00:42 +0100

    Añadir opción de acceder a la web app sin la necesidad de ir a /bp-user/index.php

--- bp-include/menu.php
+++ bp-include/menu.php
@@ -33,16 +33,59 @@ echo '
         
 ';
 
-if ($sessionlog == 1) {
-    echo '<li class="nav-item">
-    <a class="nav-link" href="/bp-user/"><i class="fas fa-user"></i> Área Personal de ' . $sessionus . '</a>
-</li>';
-} else {
-    echo '<li class="nav-item">
-    <a class="nav-link" href="/bp-user/"><i class="fas fa-user"></i> Hola, invitado</a>
-</li>';
-};
 echo '
         </ul>
+    </div>';
+if ($sessionlog == 1) {
+    echo '<a class="nav-link" href="/bp-user/"><i class="fas fa-user"></i> Área Personal de ' . $sessionus . '</a>';
+} else {
+    echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginmodal" data-backdrop="false">
+        Acceder
+      </button>';
+};
+echo '
+    <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="loginmodal" tabindex="-1" aria-labelledby="loginmodal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form name="loginform" id="loginform" method="post" action="bp-user/logger.php">
+      <div class="form-row align-items-center">
+      <div class="col-sm-3 my-1">
+        <label class="sr-only" for="inlineFormInputName">Usuario</label>
+        <input type="text" class="form-control" id="inlineFormInputName" name="usuario" placeholder="Usuario">
+      </div>
+      <div class="col-sm-3 my-1">
+        <label class="sr-only" for="inlineFormInputGroupUsername">Contraseña</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-key" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
+            <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+          </svg></div>
+          </div>
+          <input type="password" class="form-control" id="inlineFormInputGroupUsername" name="contrasena" placeholder="Contraseña">           
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Conectarse</button>
+      </div>
+      </div>
+    </div></form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
+  </div>
+</div>
 </nav>';
