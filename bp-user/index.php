c8b2292a9c3eb6680ece16b55624c31d9419fb38
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-11 21:42:59 +0100

    Actualizar aspecto formulario de Inicio de Sesión con el aspecto boostrap

--- bp-user/index.php
+++ bp-user/index.php
@@ -106,9 +106,27 @@ $qlector = mysqli_fetch_assoc($qlectorre);
         }
     } else {
         echo "<section class='bp-section'><div class='row'><div class='bp-card card-body'><form name='loginform' id='loginform' method='post' action='logger.php'>
-                    <p><strong><label>Usuario</strong><br /><input type='text' name='usuario' id='user_login' class='input' value='' /></label></p>
-                    <p><strong><label>PIN</strong><br /><input type='password' name='contrasena' id='user_pass' class='input' value=''  /></label></p>
-                    <p class='submit'><input class='btn btn-primary' type='submit' name='pwd_submit' id='pwd_submit' value='Iniciar sesi&oacute;n' /></p></form></div></div></section>";
+        <div class='form-row align-items-center'>
+        <div class='col-sm-3 my-1'>
+          <label class='sr-only' for='inlineFormInputName'>Usuario</label>
+          <input type='text' class='form-control' id='inlineFormInputName' name='usuario' placeholder='Usuario'>
+        </div>
+        <div class='col-sm-3 my-1'>
+          <label class='sr-only' for='inlineFormInputGroupUsername'>Contraseña</label>
+          <div class='input-group'>
+            <div class='input-group-prepend'>
+              <div class='input-group-text'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-key' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+              <path fill-rule='evenodd' d='M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z'/>
+              <path d='M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z'/>
+            </svg></div>
+            </div>
+            <input type='password' class='form-control' id='inlineFormInputGroupUsername' name='contrasena' placeholder='Contraseña'>           
+          </div>
+        </div>
+        <button type='submit' class='btn btn-primary'>Conectarse</button>
+        </div>
+        </div>
+      </div></form></div></div></section>";
     }
     ?>
     <footer class="page-footer bg-primary">
