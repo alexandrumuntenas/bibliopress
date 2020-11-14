18c47559241f5a8a5f76b75f54acb351e43148c0
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-14 18:32:49 +0100

    Cambio de directorios

--- bp-include/head.php
+++ bp-include/head.php
@@ -18,15 +18,15 @@ echo "<head>
     <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
     <script type='text/javascript' src='/bp-include/lives.js'></script>
     <link rel='stylesheet' type='text/css' href='/bp-include/lives.css'>
-    <script src=\"/assets/js/jquery.min.js\"></script>
-    <script src=\"/assets/bootstrap/js/bootstrap.min.js\"></script>
-    <script src=\"/assets/js/bs-init.js\"></script>
+    <script src=\"/bp-include/js/jquery.min.js\"></script>
+    <script src=\"/bp-include/bootstrap/js/bootstrap.min.js\"></script>
+    <script src=\"/bp-include/js/bs-init.js\"></script>
     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js\"></script>
-    <script src=\"/assets/js/theme.js\"></script>
-    <link rel=\"stylesheet\" href=\"/assets/bootstrap/css/bootstrap.min.css\">
+    <script src=\"/bp-include/js/theme.js\"></script>
+    <link rel=\"stylesheet\" href=\"/bp-include/bootstrap/css/bootstrap.min.css\">
     <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\">
-    <link rel=\"stylesheet\" href=\"/assets/fonts/fontawesome-all.min.css \">
-    <link rel=\"stylesheet\" href=\"/assets/fonts/font-awesome.min.css \">
-    <link rel=\"stylesheet\" href=\"/assets/fonts/fontawesome5-overrides.min.css\">
-    <link rel=\"stylesheet\" href=\"/assets/css/extra.css\">
+    <link rel=\"stylesheet\" href=\"/bp-include/fonts/fontawesome-all.min.css \">
+    <link rel=\"stylesheet\" href=\"/bp-include/fonts/font-awesome.min.css \">
+    <link rel=\"stylesheet\" href=\"/bp-include/fonts/fontawesome5-overrides.min.css\">
+    <link rel=\"stylesheet\" href=\"/bp-include/css/extra.css\">
 </head>";
