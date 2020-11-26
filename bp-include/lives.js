b24ec07c19e27c14fea812b631f1cfdc88005003
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-26 17:46:29 +0100

    Unificar archivo JS con los Scripts presentes en prestamos

--- bp-include/lives.js
+++ bp-include/lives.js
@@ -22,3 +22,41 @@ $(document).ready(function () {
         }
     });
 });
+
+function ttlibro() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("titulolibro");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tb-pres");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function uslibro() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("uslibro");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tb-pres");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
