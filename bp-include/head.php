f7b2c24c763ef56f57bb69b4ea43a8b01cce8f17
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-26 16:41:59 +0100

    Eliminar Google Fonts

--- bp-include/head.php
+++ bp-include/head.php
@@ -24,7 +24,6 @@ echo "<head>
     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js\"></script>
     <script src=\"/bp-include/js/theme.js\"></script>
     <link rel=\"stylesheet\" href=\"/bp-include/bootstrap/css/bootstrap.min.css\">
-    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\">
     <link rel=\"stylesheet\" href=\"/bp-include/fonts/fontawesome-all.min.css \">
     <link rel=\"stylesheet\" href=\"/bp-include/fonts/font-awesome.min.css \">
     <link rel=\"stylesheet\" href=\"/bp-include/fonts/fontawesome5-overrides.min.css\">
