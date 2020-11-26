336224bd1d5101b10bd759812e95257aa8af9264
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-26 17:47:09 +0100

    Creación de la Plantilla para todas las páginas

---
+++ template.php
@@ -0,0 +1,30 @@
+<?php require 'bp-config.php';
+?>
+<html>
+<title>
+    <?php echo 'Biblioteca del ' . $sname; ?>
+</title>
+<?php require 'bp-include/head.php'; ?>
+
+<body>
+    <?php require 'bp-include/menu.php'; ?>
+    <div>
+        <header>
+            <div class="bp-header">
+                <h2 class="bp-page-title">Catálogo</h2>
+            </div>
+        </header>
+        <section class="bp-section">
+            <div>
+                
+            </div>
+        </section>
+    </div>
+    </div>
+    <footer class="page-footer bg-primary">
+        <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . $dformat . " " . $sname; ?> | Powered by Bibliopress</a>
+        </div>
+    </footer>
+</body>
+
+</html>
