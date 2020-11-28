dd59379c7e3d7ae8f3c0705dae46db81651274ea
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-28 21:33:13 +0100

    Cambiar estilo del prestamo.php para adaptarlo al nuevo diseño de MDB

--- bp-admin/functions/prestamo.php
+++ bp-admin/functions/prestamo.php
@@ -26,53 +26,60 @@ if ($sessionlogged == 1) {
         require $_SERVER["DOCUMENT_ROOT"] . '/bp-include/menu.php';
         $status = "";
         if (isset($_POST['new']) && $_POST['new'] == 1) {
-        } else {
-            echo '<section class="bp-section flex-column"><div class="row">
-            <div class="bp-card card-body">
-            <h5><strong>' . $row["TITULO"] . '</strong>';
-            echo '</h5>
-            <p><em>' . $row["AUTOR"] . '</em></p>
-            <p><strong>ISBN</strong> <em>' . $row["ISBN"] . '</em></p>
-            <p><strong>Ubicación</strong> <em>' . $row["UBICACION"] . '</p></em>
-            <p><strong>Ejemplar</strong> <em>' . $row["EJEMPLAR"] . ' </em></p>
-            <p><strong>Año de Publicación</strong> <em>' . $row["ANOPUB"] . '</em></p>
-            <p><strong>Editorial</strong> <em>' . $row["EDITORIAL"] . '</em></p></div>';
-            if ($row['DISPONIBILIDAD'] == 1) {
-                echo '
-            <div class="bp-card card-body">
-            <h5><strong>Prestar libro al usuario</strong></h5>
-            <form id="form_1388" class="appnitro"  method="post" action="prestar.php?id=' . $id . '">				
-                        <ul>
-                            
-                        <li id="li_1">
-                        <label class="description" for="element_1">Nombre </label>
-                        <div>
-                            <input id="element_1" name="element_1" class="element text large" type="text" maxlength="255" value=""/> 
-                        </div> 
-                        </li>		<li id="li_2">
-                        <label class="description" for="element_2">Apellido </label>
-                        <div>
-                            <input id="element_2" name="element_2" class="element text large" type="text" maxlength="255" value=""/> 
-                        </div> 
-                        </li>
-                        <li id="li_3">
-                        <label class="description" for="element_3">Fecha de devolución </label>
-                        <div>';
-                echo date("d-m-Y", strtotime($fecha_actual . "+ 15 days"));
-                echo '
-                        </div> 
-                        </li>
-                        </ul>
-                        <div class="modal-footer">
-                            <input id="saveForm" class="btn btn-success" type="submit" name="submit" value="Prestar" />
+        } else { ?>
+            <section class="bp-section flex-column">
+                <div class="row">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row["TITULO"]; ?></h5>
+                            <p class="card-text"><em><?php echo $row["AUTOR"]; ?></em></p>
+                            <p><strong>ISBN</strong> <em><?php echo $row["ISBN"]; ?></em></p>
+                            <p><strong>Ubicación</strong> <em><?php echo $row["UBICACION"]; ?></p></em>
+                            <p><strong>Ejemplar</strong> <em><?php echo $row["EJEMPLAR"]; ?></em></p>
+                            <p><strong>Año de Publicación</strong> <em><?php echo $row["ANOPUB"]; ?></em></p>
+                            <p><strong>Editorial</strong> <em><?php echo $row["EDITORIAL"]; ?></em></p>
                         </div>
-                        </form>	
-            </div></section>';
-            } else {
-                $fnamechecksql = "SELECT * FROM `$bbddusuarios` WHERE `USUARIO` = '" . $row['PRESTADOA'] . "'";
-                $fnamedata = mysqli_query($databaseconnection, $fnamechecksql);
-                $fnamecheck = mysqli_fetch_assoc($fnamedata);
-                echo '<div class="bp-card card-body">
+                    </div>
+
+                    <?php if ($row['DISPONIBILIDAD'] == 1) { ?>
+
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Prestar libro al usuario</h5>
+                                <form id="form_1388" method="post" action="prestar.php?id=<?php echo $id; ?>">
+                                    <ul>
+
+                                        <li id="li_1">
+                                            <label class="description" for="element_1">Nombre </label>
+                                            <div>
+                                                <input id="element_1" name="element_1" class="form-control mb-4" type="text" maxlength="255" value="" />
+                                            </div>
+                                        </li>
+                                        <li id="li_2">
+                                            <label class="description" for="element_2">Apellido </label>
+                                            <div>
+                                                <input id="element_2" name="element_2" class="form-control mb-4" type="text" maxlength="255" value="" />
+                                            </div>
+                                        </li>
+                                        <li id="li_3">
+                                            <label class="description" for="element_3">Fecha de devolución </label>
+                                            <div>
+                                                <input class="form-control mb-4" value="<?php echo date("d-m-Y", strtotime($fecha_actual . "+ 15 days")); ?>" readonly />
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="modal-footer">
+                                        <input id="saveForm" class="btn btn-success" type="submit" name="submit" value="Prestar" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+            </section>
+<?php } else {
+                        $fnamechecksql = "SELECT * FROM `$bbddusuarios` WHERE `USUARIO` = '" . $row['PRESTADOA'] . "'";
+                        $fnamedata = mysqli_query($databaseconnection, $fnamechecksql);
+                        $fnamecheck = mysqli_fetch_assoc($fnamedata);
+                        echo '<div class="bp-card card-body">
                 <h5><strong>Gestionar Préstamo Activo</strong></h5>
                 <table>
                 <tbody>
@@ -98,11 +105,11 @@ if ($sessionlogged == 1) {
                             <a class="btn btn-primary" href="prorroga.php?id=' . $row["ID"] . '">Aplazar devolución</a>    <a class="btn btn-danger" style="margin-left: 10px;" href="devolver.php?id=' . $row["ID"] . '">Devolver</a>
                         </div>
                 </section>';
-            }
-        }
-    } else {
+                    }
+                }
+            } else {
 
-        echo '<section class="error-container">
+                echo '<section class="error-container">
                             <span><span>4</span></span>
                             <span>0</span>
                             <span><span>4</span></span>
@@ -110,9 +117,9 @@ if ($sessionlogged == 1) {
                           <center>
                             <h2 style="color:#FFF; margin-bottom:15px;">No tienes permiso para acceder a esta página... :/</h2>
                             <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
-    }
-} else {
-    echo '<section class="error-container">
+            }
+        } else {
+            echo '<section class="error-container">
                         <span><span>4</span></span>
                         <span>0</span>
                         <span><span>3</span></span>
@@ -120,7 +127,7 @@ if ($sessionlogged == 1) {
                       <center>
                       <h2 style="color:#FFF; margin-bottom:15px;">No tienes permiso para acceder a esta página... :/</h2>
                       <a class="btn btn-light" href="/">Llévame de vuelta</a></center>';
-};
+        };
 ?>
 
 <div>
