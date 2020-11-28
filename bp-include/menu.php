b13d942b20d8315e16e317422e91978ee1d3f6f9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-11-28 21:34:16 +0100

    Eliminar del menú el hamburguer del nav superior

--- bp-include/menu.php
+++ bp-include/menu.php
@@ -80,12 +80,8 @@
   </div>
   <div id="page-content-wrapper">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-      <button class="btn btn-primary" id="menu-toggle"><i class="fas fa-bars"></i></button>
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom ">
+      <button class="btn btn-outline-success waves-effect" id="menu-toggle"><i class="navbar-toggler-icon"></i></button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
