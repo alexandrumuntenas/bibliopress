443d314eecc13cdef992065678014bb9c2cfb6e5
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-06 18:56:25 +0100

    Quitar top bar y añadir un trigger de sidebar nuevo

--- bp-include/menu.php
+++ bp-include/menu.php
@@ -2,52 +2,36 @@
 ?>
 <div class="d-flex" id="wrapper">
   <div class="bg-light border-right " id="sidebar-wrapper">
-    <div class="sidebar-heading">Biblioteca</div>
-    <div class="list-group list-group-flush">
-      <a href="/" class="list-group-item list-group-item-action bg-light"><i class="fas fa-star"></i> Catálogo</a>
-      <a class="list-group-item list-group-item-action bg-light" data-toggle="modal" data-target="#searchmodal" data-backdrop="false"><i class="fas fa-search"></i> Búsqueda</a>
-      <?php if ($sessionlogged == 1) {
-        if ($sessionclass == 1) {
-          echo '
+    <div class="sidebar-fixed-top">
+      <div class="sidebar-heading">Biblioteca</div>
+      <div class="list-group list-group-flush">
+        <a href="/" class="list-group-item list-group-item-action bg-light"><i class="fas fa-star"></i> Catálogo</a>
+        <a class="list-group-item list-group-item-action bg-light" data-toggle="modal" data-target="#searchmodal" data-backdrop="false"><i class="fas fa-search"></i> Búsqueda</a>
+        <?php if ($sessionlogged == 1) {
+          if ($sessionclass == 1) {
+            echo '
       <a href="/bp-admin/usuarios.php" class="list-group-item list-group-item-action bg-light"><i class="fas fa-book-reader"></i> Usuarios</a>
       <a href="/bp-admin/grupos.php" class="list-group-item list-group-item-action bg-light"><i class="fas fa-users"></i> Grupos</a>
       <a href="/bp-admin/prestamos.php" class="list-group-item list-group-item-action bg-light"><i class="fas fa-people-carry"></i> Préstamos</a>
       ';
-        };
-      }; ?>
-      <?php if ($sessionlogged == 1) { ?>
-        <a class="nav-link dropdown-toggle list-group-item list-group-item-action bg-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img style="margin-right:10px;  vertical-align: middle;  width: 25px;  height: 25px;  border-radius: 50%;" src="/bp-include/avatar.png">Mi perfil</a>
-        <div class="dropdown-menu dropdown-menu-right" style="margin-right: 15px;" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/bp-user/">Mi Área Personal</a>
-          <a class="dropdown-item" href="/bp-user/miperfil.php">Mi Perfil</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="/bp-user/logout.php">Cerrar Sesión <i class="fas fa-sign-out-alt"></i></a>
-        </div>
-      <?php } else { ?>
-        <a href="" class="list-group-item list-group-item-action bg-light" data-toggle="modal" data-target="#loginmodal" data-backdrop="false"><i class="fas fa-sign-in-alt"></i> Acceder</a>
-      <?php };
-      ?>
+          };
+        }; ?>
+        <?php if ($sessionlogged == 1) { ?>
+          <a class="nav-link dropdown-toggle list-group-item list-group-item-action bg-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img style="margin-right:10px;  vertical-align: middle;  width: 25px;  height: 25px;  border-radius: 50%;" src="/bp-include/avatar.png">Mi perfil</a>
+          <div class="dropdown-menu dropdown-menu-right" style="margin-right: 15px;" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="/bp-user/">Mi Área Personal</a>
+            <a class="dropdown-item" href="/bp-user/miperfil.php">Mi Perfil</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="/bp-user/logout.php">Cerrar Sesión <i class="fas fa-sign-out-alt"></i></a>
+          </div>
+        <?php } else { ?>
+          <a href="" class="list-group-item list-group-item-action bg-light" data-toggle="modal" data-target="#loginmodal" data-backdrop="false"><i class="fas fa-sign-in-alt"></i> Acceder</a>
+        <?php };
+        ?>
+      </div>
     </div>
   </div>
   <div id="page-content-wrapper">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-      <button class="menu-collapser waves-effect" id="menu-toggle"><i class="navbar-toggler-icon"></i></button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-          <li class="nav-item dropdown">
-          <li class="nav-item navbar-nav ml-auto">
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <script>
-      $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-      });
-    </script>
     <?php if ($sessionlogged == 1) {
       if ($sessionclass == 1) { ?>
         <div class="fab-container">
@@ -83,4 +67,12 @@
           </ul>
         </div> <?php }
             } ?>
-    <?php require 'modules/modals.php'; ?>
+    <div class="sdb-collapse"><button class="menu-collapser waves-effect" id="menu-toggle"><i class="fas fa-chevron-right"></i></button>
+      </div>
+      <script>
+        $("#menu-toggle").click(function(e) {
+          e.preventDefault();
+          $("#wrapper").toggleClass("toggled");
+        });
+      </script>
+      <?php require 'modules/modals.php'; ?>
