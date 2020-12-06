9d5c294153979558b7cd050c8b02f232ea33a960
Alexandru Theodor <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-06 21:00:59 +0100

    Cambiar estilo de tablas

--- bp-user/index.php
+++ bp-user/index.php
@@ -33,8 +33,8 @@ $qlector = mysqli_fetch_assoc($qlectorre);
                     <div class="row">
                         <div class="bp-card card-body">
                             <h5>Préstamos Activos</h5>
-                            <table>
-                                <thead>
+                             <div class="table-responsive"><table class="table table-hover"   >
+                                <thead class="thead-dark" >
                                     <tr>
                                         <th>Titulo </th>
 
@@ -52,7 +52,7 @@ $qlector = mysqli_fetch_assoc($qlectorre);
                                     <tr>
                                     </tr>
                                 </tbody>
-                            </table>
+                            </table></div>
                         </div>
                         <div class="bp-card card-body">
                             <h5>En Lista de Espera</h5>
