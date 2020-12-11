28bb0c757d1625c59439bf295925ed4573b0e115
alexadrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-11 21:08:52 +0000

    Actualizar bp-settings

--- bp-settings.php
+++ bp-settings.php
@@ -3,13 +3,13 @@
                 // Valores MYSQL
                 $serverMySQL = 'localhost'; //Host de la base de datos
                 $dbMySQL = 'bibliopress'; //Nombre de la base de datos
-                $userMySQL = 'root'; //Usuario de la base de datos
+                $userMySQL = 'bibliopress'; //Usuario de la base de datos
                 $pwdMySQL = ''; //Contraseña del usuario de la base de datos
-                $prefix = 'cu1723129'; //Prefijo de tablas. No cambiar
+                $prefix = 'bp'; //Prefijo de tablas. No cambiar
 
                 // Otros parametros
                 $sname = 'Centro Centro'; //Nombre de la biblioteca/institución
-                $sitelink = 'localhost'; //Enlace del sitio web para cumplir con GDPR
+                $sitelink = '/'; //Enlace del sitio web para cumplir con GDPR
 
                 $databaseconnection = mysqli_connect($serverMySQL,$userMySQL,$pwdMySQL,$dbMySQL);
                 
