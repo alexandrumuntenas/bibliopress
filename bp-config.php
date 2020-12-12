386e93e691365de5af75114f25a433cd292dacad
alexadrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-12 13:32:01 +0000

    🎨 Mejorar estructura del código

--- bp-config.php
+++ bp-config.php
@@ -1,37 +1,39 @@
 <?php
 require('bp-settings.php');
-$formatofecha = "Y";
-$bbddusuarios = $prefix . "_usuarios";
+# Querry Libros
 $bbddcatalogo = $prefix . "_catalogo";
-$bbddsesiones = $prefix . "_sesiones";
+$librosql = "SELECT * FROM $bbddcatalogo";
+$libroquerry = mysqli_query($databaseconnection,$librosql);
+$libroresultado = mysqli_fetch_assoc($libroquerry);
+# Querry Lectores
+$bbddusuarios = $prefix . "_usuarios";
+$usuariosql = "SELECT * FROM `$bbddusuarios`";
+$usuarioquerry = mysqli_query($databaseconnection,$usuariosql);
+$usuarioresultado = mysqli_fetch_assoc($usuarioquerry);
+# Querry Grupos
 $bbddgrupos = $prefix . "_grupo";
-$sql = "SELECT TITULO, AUTOR, ISBN, EDITORIAL, UBICACION, ANOPUB, EJEMPLAR, ID, DESCRIPCION, DISPONIBILIDAD, PRESTADOA, FECHADEV FROM $bbddcatalogo";
-$resultado = $databaseconnection->query($sql);
-$lectorsql = "SELECT * FROM `$bbddusuarios`";
-$lectorresultado = $databaseconnection->query($lectorsql);
-$lectordata = $databaseconnection->query($lectorsql);
-$lector = mysqli_fetch_assoc($lectordata);
-$numerolibros = mysqli_num_rows($resultado);
-$qtyprestadosql = "SELECT *  FROM `$bbddcatalogo` WHERE `DISPONIBILIDAD` = 0";
-$qtyprestadoquery = $databaseconnection->query($qtyprestadosql);
-$qtyprestados = mysqli_num_rows($qtyprestadoquery);
-$aq112 = mysqli_fetch_assoc($resultado);
-date_default_timezone_set('Europe/Berlin');
-$fecha_actual = date('m/d/Y');
-$timestamp = date("Y-m-d", strtotime($fecha_actual . "+ 15 days"));
-$prorrogafecha = date("Y-m-d", strtotime($aq112['FECHADEV'] . "+ 15 days"));
-echo mysqli_error($databaseconnection);
-#ini_set('display_errors', 0);
-#error_reporting(0);
+$gruposql = "SELECT * FROM `$bbddgrupos`";
+$grupoquerry = mysqli_query($databaseconnection, $gruposql);
+$gruporesultado = mysqli_fetch_assoc($grupoquerry);
+# Querry Sesiones
+$bbddsesiones = $prefix . "_sesiones";
+$sesionessql = "SELECT * FROM `$bbddsesiones`";
+$sesionesquerry = mysqli_fetch_assoc($databaseconnection);
+$sessionlogged = $sesionesquerry['LOGGEDIN'];
+$sessionclass = $sesionesquerry['PERM'];
+$sessionus = $sesionesquerry['USUARIO'];
 $phpsessid = session_id();
-$secureloginsql = "SELECT * FROM `$bbddsesiones`";
-$secureloginquery = $databaseconnection->query($secureloginsql);
-$secureloginresult = mysqli_fetch_assoc($secureloginquery);
-$sessionlogged = $secureloginresult['LOGGEDIN'];
-$sessionclass = $secureloginresult['PERM'];
-$sessionus = $secureloginresult['USUARIO'];
 if ($phpsessid == null) {
     session_start();
     $phpsessid = session_id();
 }
-$gruposql = mysqli_query($databaseconnection, "SELECT * FROM `$bbddgrupos`");
+#Other PHP Resources
+echo mysqli_error($databaseconnection);
+$fecha_actual = date('m/d/Y');
+$formatofecha = "Y";
+$timestamp = date("Y-m-d", strtotime($fecha_actual . "+ 15 days"));
+$prorrogafecha = date("Y-m-d", strtotime($aq112['FECHADEV'] . "+ 15 days"));
+#Default Settings
+date_default_timezone_set('Europe/Berlin');
+#ini_set('display_errors', 0);
+#error_reporting(0);
