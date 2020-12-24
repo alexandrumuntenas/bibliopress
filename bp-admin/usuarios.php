fedb0648f867b04b8903d0b42479a1d5855146c2
alexandrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-24 18:12:42 +0000

    🎨 Añadir parámetro grupo a la navegación

--- bp-admin/usuarios.php
+++ bp-admin/usuarios.php
@@ -22,13 +22,24 @@ if ($sessionlogged == 1) {
                     $compag         = (int)(!isset($_GET['pag'])) ? 1 : $_GET['pag'];
                     if (isset($_GET['grupo'])) {
                         $gr = $_GET['grupo'];
+                        if ($gr != NULL) {
                         $TotalReg       = $databaseconnection->query("SELECT * FROM `$bbddusuarios` WHERE `CLASE` LIKE '$gr'");
                         $TotalRegistro  = ceil($TotalReg->num_rows / $CantidadMostrar);
                         $consultavistas = "SELECT * FROM `$bbddusuarios` WHERE `CLASE` LIKE '$gr'
                                     ORDER BY
                                     id ASC
                                     LIMIT " . (($compag - 1) * $CantidadMostrar) . " , " . $CantidadMostrar;
-                        $consulta = $databaseconnection->query($consultavistas);
+                        $consulta = $databaseconnection->query($consultavistas); } else {
+                            $TotalReg       = mysqli_query($databaseconnection, "SELECT * FROM `$bbddusuarios`");
+                            $TotalRegistro  = ceil($TotalReg->num_rows / $CantidadMostrar);
+                            $consultavistas = "SELECT * FROM `$bbddusuarios`
+                                    ORDER BY
+                                    id ASC
+                                    LIMIT " . (($compag - 1) * $CantidadMostrar) . " , " . $CantidadMostrar;
+                            $consulta = mysqli_query($databaseconnection, $consultavistas);
+
+                        }
+
                     } else {
                         $TotalReg       = mysqli_query($databaseconnection,"SELECT * FROM `$bbddusuarios`");
                         $TotalRegistro  = ceil($TotalReg->num_rows / $CantidadMostrar);
@@ -45,6 +56,7 @@ if ($sessionlogged == 1) {
                         Búsqueda de usuarios por grupo > &nbsp
                         </br>
                         <select class="form-control form-control-sm" name="grupo" id="">
+                            <option value="No asignado">No asignado</option>
                             <?php
                             mysqli_data_seek($grupoquery, 0);
                             if ($grupoquery->num_rows > 0) {
@@ -90,7 +102,7 @@ if ($sessionlogged == 1) {
                     <?php
                     $IncrimentNum = (($compag + 1) <= $TotalRegistro) ? ($compag + 1) : 1;
                     $DecrementNum = (($compag - 1)) < 1 ? 1 : ($compag - 1);
-                    echo "<ul class='pagination pg-blue'><li class=\"page-item\"><a class='page-link' href=\"?pag=" . $DecrementNum . "\">&laquo;</a></li>";
+                    echo "<ul class='pagination pg-blue'><li class=\"page-item\"><a class='page-link' href=\"?grupo=$gr&pag=" . $DecrementNum . "\">&laquo;</a></li>";
                     $Desde = $compag - (ceil($CantidadMostrar / 2) - 1);
                     $Hasta = $compag + (ceil($CantidadMostrar / 2) - 1);
                     $Desde = ($Desde < 1) ? 1 : $Desde;
@@ -98,13 +110,13 @@ if ($sessionlogged == 1) {
                     for ($i = $Desde; $i <= $Hasta; $i++) {
                         if ($i <= $TotalRegistro) {
                             if ($i == $compag) {
-                                echo "<li class=\"page-item active\"><a class='page-link' href=\"?pag=" . $i . "\">" . $i . "</a></li>";
+                                echo "<li class=\"page-item active\"><a class='page-link' href=\"?grupo=$gr&pag=" . $i . "\">" . $i . "</a></li>";
                             } else {
-                                echo "<li class=\"page-item\"><a class='page-link' href=\"?pag=" . $i . "\">" . $i . "</a></li>";
+                                echo "<li class=\"page-item\"><a class='page-link' href=\"?grupo=$gr&pag=" . $i . "\">" . $i . "</a></li>";
                             }
                         }
                     }
-                    echo "<li class=\"page-item\"><a class='page-link' href=\"?pag=" . $IncrimentNum . "\">&raquo;</a></li></ul>";
+                    echo "<li class=\"page-item\"><a class='page-link' href=\"?grupo=$gr&pag=" . $IncrimentNum . "\">&raquo;</a></li></ul>";
                     ?>
             </div>
             </div>
