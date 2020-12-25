656880580c4aa54d337eed794e3a28e30bdeb48f
alexandrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-25 00:05:13 +0000

    🐛 BUG: Reporting 403

--- bp-user/index.php
+++ bp-user/index.php
@@ -1,5 +1,4 @@
-<?php require '../bp-config.php';
-?>
+<?php ?>
 <html>
 <title>
     <?php echo 'Biblioteca del ' . $sname; ?>
@@ -16,15 +15,15 @@ $qlector = mysqli_fetch_assoc($qlectorre);
 ?>
 
 <body>
-    <?php if ($sessionlogged == 1) { ?>
-        <?php require '../bp-include/menu.php'; ?>
-        <div>
-            <header>
-                <div class="bp-header">
-                    <h2 class="bp-page-title">Mi Área Personal</h2>
-                </div>
-            </header>
-            <section class="bp-section">
+    <div>
+        <header>
+            <div class="bp-header">
+                <h2 class="bp-page-title">Mi Área Personal</h2>
+            </div>
+        </header>
+        <section class="bp-section">
+            <?php if ($sessionlogged == 1) { ?>
+
                 <center>
                     <a href="index.php" type="button" class="btn btn-primary">Inicio</a>
                     <a href="miperfil.php" type="button" class="btn btn-secondary">Mi Perfil <i class="fas fa-id-card-alt"></i></a>
@@ -81,39 +80,12 @@ $qlector = mysqli_fetch_assoc($qlectorre);
                         <h5>Tus Últimas Lecturas</h5>
                         <p>No te emociones, seguimos trabajando en ello</p>
                     </div>
-                    <?php if ($sessionclass == 1) {
-                        echo '
-                            <div class="bp-card card-body">
-                                <h5>Sobre la Biblioteca</h5>
-                                <p>Biblioteca del ' . $sname . '</p>
-                                <p>Hay un total de ' . $numerolibros . ' libros en todo el catálogo, de los cuales, ' . $qtyprestados . ' están prestados</p>
-                            </div>
-                            ';
-                    } ?>
-                </div>
-            </section>
-        </div>
+                </div> <?php } else { ?> <p>No tienes permiso para acceder a esta página</p><?php } ?>
         </section>
-        </div>
-        </div>
-        </div>
-        </div>
-    <?php } else { ?>
-
-        <body class="err403">
-            <section class="error-container">
-                <span><span>4</span></span>
-                <span>0</span>
-                <span><span>3</span></span>
-            </section>
-            <center>
-                <h2 style="color:#FFF; margin-bottom:15px;">No tienes permiso para acceder a esta página... :/</h2>
-                <a class="btn btn-light" href="/">Llévame de vuelta</a>
-            </center><?php } ?>
         <footer class="page-footer bg-primary">
             <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . date("Y") . " " . $sname; ?> | Powered by Bibliopress</a>
             </div>
         </footer>
-        </body>
+</body>
 
 </html>
--- bp-user/miperfil.php
+++ bp-user/miperfil.php
@@ -1,5 +1,4 @@
-<?php require '../bp-config.php';
-$querylector = "SELECT *  FROM `$bbddusuarios` WHERE `USUARIO` LIKE '" . $sessionus . "'";
+<?php $querylector = "SELECT *  FROM `$bbddusuarios` WHERE `USUARIO` LIKE '" . $sessionus . "'";
 $qlectorre = mysqli_query($databaseconnection, $querylector);
 $qlector = mysqli_fetch_assoc($qlectorre);
 
@@ -26,88 +25,67 @@ if (isset($_POST['submit'])) {
 <title>
     <?php echo 'Biblioteca del ' . $sname; ?>
 </title>
-<?php require '../bp-include/head.php'; ?>
-
+<?php require '../bp-include/head.php';
+?>
 <body>
-    <?php if ($sessionlogged == 1) { ?>
-        <?php require '../bp-include/menu.php'; ?>
-        <div>
-            <header>
-                <div class="bp-header">
-                    <h2 class="bp-page-title">Mi Perfil</h2>
-                </div>
-            </header>
-            <section class="bp-section">
-                <div>
-                    <center>
-                        <a href="index.php" type="button" class="btn btn-primary">Inicio</a>
-                        <a href="miperfil.php" type="button" class="btn btn-secondary">Mi Perfil <i class="fas fa-id-card-alt"></i></a>
-                        <a href="?logout" type="button" class="btn btn-danger">Cerrar Sesión <i class="fas fa-sign-out-alt"></i></a>
-                    </center>
-                    <div class="row">
-                        <div class="bp-card card-body">
-                            <h5>Sobre Mí</h5>
-                            <form id="form_1388" method="post" action="">
-                                <ul>
-
-                                    <li id="li_1">
-                                        <label class="description" for="element_1">Nombre </label>
-                                        <div>
-                                            <input id="element_1" name="element_1" class="form-control form-control-sm" type="text" maxlength="255" value="<?php echo $qlector['NOMBRE']; ?>" readonly />
-                                        </div>
-                                    </li>
-                                    <li id="li_2">
-                                        <label class="description" for="element_2">Apellido </label>
-                                        <div>
-                                            <input id="element_2" name="element_2" class="form-control form-control-sm" type="text" maxlength="255" value="<?php echo $qlector['APELLIDOS']; ?>" readonly />
-                                        </div>
-                                    </li>
-                                    <li id="li_2">
-                                        <label class="description" for="element_5">Cambiar Correo Electrónico</label><small>Correo Actual</small>
-                                        <div>
-                                            <input id="element_5" name="element_5" class="form-control form-control-sm" type="email" maxlength="255" value="<?php echo $sessionus; ?>" />
-                                        </div>
-                                        <small>Correo Nuevo</small>
-                                        <div>
-                                            <input id="element_6" name="element_6" class="form-control form-control-sm" type="email" maxlength="255" value="" />
-                                        </div>
-                                    </li>
-                                </ul>
-                                <input type="submit">
-                            </form>
-                        </div>
-                        <div class="bp-card card-body">
-                            <h5>Mi Avatar</h5>
-                            <p>No te emociones, seguimos trabajando en ello</p>
-                        </div>
-                        <div class="bp-card card-body">
-                            <h5>Notificaciones</h5>
-                            <p>No te emociones, seguimos trabajando en ello</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+    <header>
+        <div class="bp-header">
+            <h2 class="bp-page-title">Mi Perfil</h2>
         </div>
-        </div>
-        </div>
-        </div>
-    <?php } else { ?>
-
-        <body class="err403">
-            <section class="error-container">
-                <span><span>4</span></span>
-                <span>0</span>
-                <span><span>3</span></span>
-            </section>
+    </header>
+    <section class="bp-section">
+        <?php if ($sessionlogged == 1) { ?>
             <center>
-                <h2 style="color:#FFF; margin-bottom:15px;">No tienes permiso para acceder a esta página... :/</h2>
-                <a class="btn btn-light" href="/">Llévame de vuelta</a>
+                <a href="index.php" type="button" class="btn btn-primary">Inicio</a>
+                <a href="miperfil.php" type="button" class="btn btn-secondary">Mi Perfil <i class="fas fa-id-card-alt"></i></a>
+                <a href="?logout" type="button" class="btn btn-danger">Cerrar Sesión <i class="fas fa-sign-out-alt"></i></a>
             </center>
-        <?php } ?>
-        <footer class="page-footer bg-primary">
-            <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . date("Y") . " " . $sname; ?> | Powered by Bibliopress</a>
+            <div class="row">
+                <div class="bp-card card-body">
+                    <h5>Sobre Mí</h5>
+                    <form id="form_1388" method="post" action="">
+                        <ul>
+
+                            <li id="li_1">
+                                <label class="description" for="element_1">Nombre </label>
+                                <div>
+                                    <input id="element_1" name="element_1" class="form-control form-control-sm" type="text" maxlength="255" value="<?php echo $qlector['NOMBRE']; ?>" readonly />
+                                </div>
+                            </li>
+                            <li id="li_2">
+                                <label class="description" for="element_2">Apellido </label>
+                                <div>
+                                    <input id="element_2" name="element_2" class="form-control form-control-sm" type="text" maxlength="255" value="<?php echo $qlector['APELLIDOS']; ?>" readonly />
+                                </div>
+                            </li>
+                            <li id="li_2">
+                                <label class="description" for="element_5">Cambiar Correo Electrónico</label><small>Correo Actual</small>
+                                <div>
+                                    <input id="element_5" name="element_5" class="form-control form-control-sm" type="email" maxlength="255" value="<?php echo $sessionus; ?>" />
+                                </div>
+                                <small>Correo Nuevo</small>
+                                <div>
+                                    <input id="element_6" name="element_6" class="form-control form-control-sm" type="email" maxlength="255" value="" />
+                                </div>
+                            </li>
+                        </ul>
+                        <input type="submit">
+                    </form>
+                </div>
+                <div class="bp-card card-body">
+                    <h5>Mi Avatar</h5>
+                    <p>No te emociones, seguimos trabajando en ello</p>
+                </div>
+                <div class="bp-card card-body">
+                    <h5>Notificaciones</h5>
+                    <p>No te emociones, seguimos trabajando en ello</p>
+                </div>
             </div>
-        </footer>
-        </body>
+    </section> <?php } else { ?> <p>No tienes permiso para acceder a esta página</p><?php } ?>
+<footer class="page-footer bg-primary">
+    <div class="footer-copyright text-center py-3 fwhite"><?php echo "© " . date("Y") . " " . $sname; ?> | Powered by Bibliopress</a>
+    </div>
+</footer>
+</body>
 
 </html>
