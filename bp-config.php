cf6d33e22f68fda2bc4459b80f228ecc0326b40a
alexandrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-25 16:57:17 +0000

    🎨 Mejorar organización de bp-config y añadir nueva bbdd

--- bp-config.php
+++ bp-config.php
@@ -9,24 +9,27 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 } else {
     $ip_address = $_SERVER['REMOTE_ADDR'];
 }
-#Query Libros
+#Bases de Datos
 $bbddcatalogo = $prefix . "_catalogo";
+$bbddusuarios = $prefix . "_usuarios";
+$bbddgrupos = $prefix . "_grupo";
+$bbddsesiones = $prefix . "_sesiones";
+$bbddcomentarios = $prefix . "_comentarios";
+$bbddsolicitudes = $prefix . "_solicitudes";
+#Libros
 $librosql = "SELECT * FROM $bbddcatalogo";
 $libroquery = mysqli_query($databaseconnection, $librosql);
 $libroresultado = mysqli_fetch_assoc($libroquery);
-#Query Lectores
-$bbddusuarios = $prefix . "_usuarios";
+#Lectores
 $usuariosql = "SELECT * FROM `$bbddusuarios`";
 $usuarioquery = mysqli_query($databaseconnection, $usuariosql);
 $usuarioresultado = mysqli_fetch_assoc($usuarioquery);
-#Query Grupos
-$bbddgrupos = $prefix . "_grupo";
+#Grupos
 $gruposql = "SELECT * FROM `$bbddgrupos`";
 $grupoquery = mysqli_query($databaseconnection, $gruposql);
 $gruporesultado = mysqli_fetch_assoc($grupoquery);
-#Query Sesiones
+#Sesiones
 $phpsessid = mysqli_real_escape_string($databaseconnection, $_COOKIE['PHPSESSID']);
-$bbddsesiones = $prefix . "_sesiones";
 $sesionessql = "SELECT * FROM `$bbddsesiones` WHERE `PHPSESSID` LIKE '$phpsessid' AND `IP` LIKE '$ip_address' AND `USER_AGENT` LIKE '$uagent'";
 $sesionesquery = mysqli_query($databaseconnection, $sesionessql);
 $sesionesresultado = mysqli_fetch_assoc($sesionesquery);
@@ -41,8 +44,8 @@ if ($phpsessid == null) {
 $sesavatarsql = "SELECT * FROM `$bbddusuarios` WHERE USUARIO LIKE '$sessionus'";
 $sesavatarquery = mysqli_query($databaseconnection, $sesavatarsql);
 $sesavatarresultado = mysqli_fetch_assoc($sesavatarquery);
-#Query Comentarios
-$bbddcomentarios = $prefix . "_comentarios";
+#Comentarios
+#Solicitudes
 #Other PHP Resources
 echo mysqli_error($databaseconnection);
 $fecha_actual = date('m/d/Y');
