8a27dfd74282e3c5961578d1252365e503391148
alexandrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2020-12-25 17:19:07 +0000

    🎨 Poner paginación, mejorar código

--- bp-admin/prestamos.php
+++ bp-admin/prestamos.php
@@ -1,5 +1,5 @@
 <?php require '../bp-include/head.php';
-$prestamosql = "SELECT * FROM $bbddcatalogo WHERE `DISPONIBILIDAD` LIKE 0";
+$prestamosql = "SELECT * FROM $bbddcatalogo WHERE `DISPONIBILIDAD` LIKE 0 OR `DISPONIBILIDAD` LIKE 3";
 $prestamoquery = mysqli_query($databaseconnection, $prestamosql);
 ?>
 
@@ -17,7 +17,16 @@ $prestamoquery = mysqli_query($databaseconnection, $prestamosql);
         <section class="bp-section">
             <div>
                 <?php if ($sessionlogged == 1) {
-                    if ($sessionclass == 1) { ?>
+                    if ($sessionclass == 1) {
+                        $compag         = (int)(!isset($_GET['pag'])) ? 1 : $_GET['pag'];
+                        $TotalReg       = $databaseconnection->query("SELECT * FROM $bbddcatalogo WHERE `DISPONIBILIDAD` LIKE 0 OR `DISPONIBILIDAD` LIKE 3");
+                        $TotalRegistro  = ceil($TotalReg->num_rows / $CantidadMostrar);
+                        $consultavistas = "SELECT * FROM `$bbddcatalogo` WHERE `DISPONIBILIDAD` LIKE 0 OR WHERE `DISPONIBILIDAD` LIKE 3 
+                                    ORDER BY
+                                    id ASC
+                                    LIMIT " . (($compag - 1) * $CantidadMostrar) . " , " . $CantidadMostrar;
+                        $consulta = $databaseconnection->query($consultavistas);
+                        ?>
                         <input class="inputbusqueda" type="text" id="titulolibro" onkeyup="ttlibro()" placeholder="Busca por título del libro..." title="Escribe el título del libro">
                         <div class="lectores">
                             <div class="table-responsive">
@@ -43,15 +52,39 @@ $prestamoquery = mysqli_query($databaseconnection, $prestamosql);
                                 <td data-label="Título del libro"><br>' . $prestamorow["TITULO"] . '</td>
                                 <td data-label="Fecha de devolución"><br>' . $prestamorow["FECHADEV"] . '</td>
                                 <td data-label="Título prestado al usuario "><br>' . $tomarnombrerow['FULLNAME'] . '</td>
-                                <td data-label="Acciones disponibles"><br><a style="color:blue;" href="functions/prorroga.php?id=' . $prestamorow["ID"] . '">Aplazar devolución</a>    <a style="color:green;" href="functions/devolver.php?id=' . $prestamorow["ID"] . '">Devolver</a></td>
+                                <td data-label="Acciones disponibles"><br><a style="color:blue;" href="?prorroga=' . $prestamorow["ID"] . '">Aplazar devolución</a>    <a style="color:green;" href="?devolver=' . $prestamorow["ID"] . '">Devolver</a></td>
                         </tr>';
                                             }
-                                        } ?>
+                                        } else { ?>
+                                            <tr>
+                                                <td>No hay préstamos activos</td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-                    <?php } else { ?>
+                        <?php $IncrimentNum = (($compag + 1) <= $TotalRegistro) ? ($compag + 1) : 1;
+                        $DecrementNum = (($compag - 1)) < 1 ? 1 : ($compag - 1);
+                        echo "<ul class='pagination pg-blue'><li class=\"page-item\"><a class='page-link' href=\"?pag=" . $DecrementNum . "\">&laquo;</a></li>";
+                        $Desde = $compag - (ceil($CantidadMostrar / 2) - 1);
+                        $Hasta = $compag + (ceil($CantidadMostrar / 2) - 1);
+                        $Desde = ($Desde < 1) ? 1 : $Desde;
+                        $Hasta = ($Hasta < $CantidadMostrar) ? $CantidadMostrar : $Hasta;
+                        for ($i = $Desde; $i <= $Hasta; $i++) {
+                            if ($i <= $TotalRegistro) {
+                                if ($i == $compag) {
+                                    echo "<li class=\"page-item active\"><a class='page-link' href=\"?pag=" . $i . "\">" . $i . "</a></li>";
+                                } else {
+                                    echo "<li class=\"page-item\"><a class='page-link' href=\"?pag=" . $i . "\">" . $i . "</a></li>";
+                                }
+                            }
+                        }
+                        echo "<li class=\"page-item\"><a class='page-link' href=\"?pag=" . $IncrimentNum . "\">&raquo;</a></li></ul>";
+                        } else { ?>
                         <p>No tienes permiso para acceder a esta página</p>
                     <?php }
                 } else { ?>
